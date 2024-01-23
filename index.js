ce872b68396ff4a2ba341e9fe19e02726d1819d6
Penkuza <artem.balabanov.2017@gmail.com>
2024-01-23 16:24:14 +0300

    new folder

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function animate (){
 
     c.fillStyle = 'white'
     c.fillRect(0,0,canvas.width,canvas.height)
-    player.velocity.x=0;
+    player.velocity.x=0
     if(keys.d.pressed)player.velocity.x = 5
     else if(keys.a.pressed)player.velocity.x =-5
     
@@ -32,32 +32,3 @@ function animate (){
 }
 
 animate()
-
-window.addEventListener('keydown', (event)=>{
-    switch(event.key){
-        case 'w':
-            if(player.velocity.y ===0) player.velocity.y=-20   
-        break
-            case 'a':
-                keys.a.pressed =true
-            break
-                case 'd':
-                    keys.d.pressed =true
-                break
-            
-    }
-})
-window.addEventListener('keyup', (event)=>{
-    switch(event.key){
-
-            case 'a':
-                keys.a.pressed =false
-
-            break
-                case 'd':
-                    keys.d.pressed =false
-
-                break
-            
-    }
-})
