c448679581291c37106bf32e802195867d76a2bb
Penkuza <artem.balabanov.2017@gmail.com>
2024-01-23 14:16:30 +0300

    add animate

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d');
 
-canvas.width = 1024
-canvas.height = 576
+canvas.width = 64 * 16
+canvas.height = 64 * 9
 
+let y = 100
+function animate (){
+window.requestAnimationFrame(animate)
 c.fillStyle = 'white'
-c.fillRect(0,0,canvas.width,canvas.height)
+c.clearRect(0,0,canvas.width.canvas.height)
+c.fillStyle ='red'
+c.fillRect(100, y, 100, 100) 
+y++
+}
+
+animate()
