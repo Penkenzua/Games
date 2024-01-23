dc8ea15b49b6290171af06010d7cb2dba9c7cf21
Penkuza <artem.balabanov.2017@gmail.com>
2024-01-23 17:22:10 +0300

    background processing

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const c = canvas.getContext('2d');
 canvas.width = 64 * 16
 canvas.height = 64 * 9
 
-
+const backgroundLevel1 = new Sprite({
+    position:{
+      x:0,
+      y:0,
+    }, 
+    imageSrc: 'img/backgroundLevel1.png'
+  })
 const player = new Player()
 const keys = {
     w: {
@@ -19,9 +25,8 @@ const keys = {
 }
 function animate (){
     window.requestAnimationFrame(animate)
+    backgroundLevel1.draw()
 
-    c.fillStyle = 'white'
-    c.fillRect(0,0,canvas.width,canvas.height)
     player.velocity.x=0
     if(keys.d.pressed)player.velocity.x = 5
     else if(keys.a.pressed)player.velocity.x =-5
--- js/classes/Sprite.js
+++ js/classes/Sprite.js
@@ -1,8 +1,15 @@
 class Sprite{
-    constructor({position}){
-        this.position = this.position
+    constructor({position,imageSrc}){
+        this.position = position
         this.image = new Image()
-        this.image.src =''
+        this.image.onload =() =>{
+            this.loaded =true
+        }
+        this.image.src = imageSrc
+        this.loaded = false
     }
-
+    draw(){
+        if (!this.loaded)return
+        c.drawImage(this.image,this.position.x,this.position.y)
+    }
 }
