cb9b815e5de3c944fd1b87a7624ff7ac61a5eb1e
Penkuza <artem.balabanov.2017@gmail.com>
2024-01-23 16:19:23 +0300

    added the ability to move

--- index.js
+++ index.js
@@ -6,14 +6,58 @@ canvas.height = 64 * 9
 
 
 const player = new Player()
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+}
 function animate (){
     window.requestAnimationFrame(animate)
 
     c.fillStyle = 'white'
-    c.fillRect(0,0,canvas.width,canvas.height) 
+    c.fillRect(0,0,canvas.width,canvas.height)
+    player.velocity.x=0;
+    if(keys.d.pressed)player.velocity.x = 5
+    else if(keys.a.pressed)player.velocity.x =-5
+    
     player.draw()
     player.update()
 
 }
 
 animate()
+
+window.addEventListener('keydown', (event)=>{
+    switch(event.key){
+        case 'w':
+            if(player.velocity.y ===0) player.velocity.y=-20   
+        break
+            case 'a':
+                keys.a.pressed =true
+            break
+                case 'd':
+                    keys.d.pressed =true
+                break
+            
+    }
+})
+window.addEventListener('keyup', (event)=>{
+    switch(event.key){
+
+            case 'a':
+                keys.a.pressed =false
+
+            break
+                case 'd':
+                    keys.d.pressed =false
+
+                break
+            
+    }
+})
--- js/classes/Player.js
+++ js/classes/Player.js
@@ -5,21 +5,31 @@ class Player{
             x:100,
             y:100
         }
+        this.velocity ={
+            x:0,
+            y:0,
+        }
         this.width =100
         this.height = 100
         this.sides = {
             bottom: this.position.y + this.height
         }
+        this.gravity =1
     }
     draw(){
         c.fillStyle ='red'
         c.fillRect(this.position.x, this.position.y, this.width, this.height) 
     }
     update(){
-      if(this.sides.bottom< canvas.height){
-        this.position.y++
-        this.sides.bottom=this.position.y + this.height
-      } 
+    this.position.x += this.velocity.x
+    this.position.y += this.velocity.y
+    this.sides.bottom=this.position.y + this.height
+    if(this.sides.bottom + this.velocity.y < canvas.height){
+        //Добавление силы гравитации
+        this.velocity.y +=this.gravity
+      }
+      //Сила гравитации/движения = 0 если достигли конца уровня
+      else this.velocity.y=0 
 
     }
 }
