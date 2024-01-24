636a18356fa30c7ea01fff9547f87177ec9020ac
Penkuza <artem.balabanov.2017@gmail.com>
2024-01-24 21:16:18 +0300

    Рефакторинг Player

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -24,55 +24,58 @@ class Player{
     update(){
     this.position.x += this.velocity.x
     //Проверка горизонтальных столкновений
-    for (let i = 0; i < this.collisionBlocks.length; i++) {
-        
-         const collisionBlock = this.collisionBlocks[i]
-         if(this.position.x<=collisionBlock.position.x+collisionBlock.width &&
-            this.position.x +this.width>=collisionBlock.position.x &&
-            this.position.y +this.height >= collisionBlock.position.y &&
-            this.position.y<= collisionBlock.position.y + collisionBlock.height
-            ){
-                if(this.velocity.x<-0){
-                    this.position.x=collisionBlock.position.x +collisionBlock.width+0.01
-                    break
-                }
-                if (this.velocity.x>0){
-                    this.position.x= collisionBlock.position.x - this.width - 0.01
-                    break
-                }
-         }
+    this.checkForHorizontalCollisions()
+    this.applyGravity()
+    this.checkForVerticalCollosions()
     }
+
+    
+    applyGravity(){
     //Применение гравитации
+    this.velocity.y +=this.gravity
     this.position.y += this.velocity.y
-    this.sides.bottom=this.position.y + this.height
-
-    for (let i = 0; i < this.collisionBlocks.length; i++) {
+    }
+    checkForHorizontalCollisions(){
+        for (let i = 0; i < this.collisionBlocks.length; i++) {
         
-        const collisionBlock = this.collisionBlocks[i]
-        if(this.position.x<=collisionBlock.position.x+collisionBlock.width &&
-           this.position.x +this.width>=collisionBlock.position.x &&
-           this.position.y +this.height >= collisionBlock.position.y &&
-           this.position.y<= collisionBlock.position.y + collisionBlock.height
-           ){
-               if(this.velocity.y< 0){
-                    this.velocity.y=0
-                   this.position.y=collisionBlock.position.y +collisionBlock.height +0.01
-                   break
-               }
-               if (this.velocity.y>0){
-                    this.velocity.y=0
-                   this.position.y= collisionBlock.position.y - this.width - 0.01
-                   break
-               }
-        }
-   }
-
-    if(this.sides.bottom + this.velocity.y < canvas.height){
-        //Добавление силы гравитации
-        this.velocity.y +=this.gravity
-      }
-      //Сила гравитации/движения = 0 если достигли конца уровня
-      else this.velocity.y=0 
+            const collisionBlock = this.collisionBlocks[i]
+            if(this.position.x<=collisionBlock.position.x+collisionBlock.width &&
+               this.position.x +this.width>=collisionBlock.position.x &&
+               this.position.y +this.height >= collisionBlock.position.y &&
+               this.position.y<= collisionBlock.position.y + collisionBlock.height
+               ){
+                   if(this.velocity.x<-0){
+                       this.position.x=collisionBlock.position.x +collisionBlock.width+0.01
+                       break
+                   }
+                   if (this.velocity.x>0){
+                       this.position.x= collisionBlock.position.x - this.width - 0.01
+                       break
+                   }
+            }
+       }
+    }
+    checkForVerticalCollosions(){
+        for (let i = 0; i < this.collisionBlocks.length; i++) {
+        
+            const collisionBlock = this.collisionBlocks[i]
+            if(this.position.x<=collisionBlock.position.x+collisionBlock.width &&
+               this.position.x +this.width>=collisionBlock.position.x &&
+               this.position.y +this.height >= collisionBlock.position.y &&
+               this.position.y<= collisionBlock.position.y + collisionBlock.height
+               ){
+                   if(this.velocity.y< 0){
+                        this.velocity.y=0
+                       this.position.y=collisionBlock.position.y +collisionBlock.height +0.01
+                       break
+                   }
+                   if (this.velocity.y>0){
+                        this.velocity.y=0
+                       this.position.y= collisionBlock.position.y - this.width - 0.01
+                       break
+                   }
+            }
+       }
 
     }
 } 
